71548c8ea37c24ed059030662703acf0c930a20a
Scott <scottflack@covergenius.com>
2025-06-18 16:29:12 +1000

    adding logging via php-logger

--- src/Shell/ShellLogListener.php
+++ src/Shell/ShellLogListener.php
@@ -2,21 +2,31 @@
 
 namespace CoverGenius\Tinker\Shell;
 
+use Covergenius\Php\Logger\Resources\CodeExecutionLogger;
 use Psy\ExecutionLoop\AbstractListener;
 use Psy\Shell;
 
 class ShellLogListener extends AbstractListener
 {
     /**
+     * @var CodeExecutionLogger|null
+     */
+    protected ?CodeExecutionLogger $logger;
+
+    /**
      * Listen for code execution.
      *
-     * @param  \Psy\Shell  $shell
+     * @param Shell $shell
      * @param  string  $code
      * @return void
      */
-    public function onExecute(Shell $shell, $code)
+    public function onExecute(Shell $shell, string $code): void
     {
-        \Log::debug('Tinker session code run', ['code' => $code]);
+        if ($this->logger instanceof CodeExecutionLogger) {
+            $this->logger->addToLog($code);
+        } else {
+            $this->logger = CodeExecutionLogger::startLog($code);
+        }
     }
 
     /**
@@ -27,5 +37,20 @@ class ShellLogListener extends AbstractListener
     public static function isSupported(): bool
     {
         return true; //implement a proper check
+    }
+
+    /**
+     * Commits the log if the logger has been initialized.
+     *
+     * This ensures that the log is always saved, even if the shell is exited
+     * or the process is terminated.
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        if ($this->logger instanceof CodeExecutionLogger) {
+            $this->logger->commitLog();;
+        }
     }
 }
