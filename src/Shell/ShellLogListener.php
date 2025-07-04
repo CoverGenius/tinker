ac6674c550df609c0fe198865cd9a7f606d69485
Scott <scottflack@covergenius.com>
2025-07-04 16:19:03 +1000

    change to use arbitrary code logger

--- src/Shell/ShellLogListener.php
+++ src/Shell/ShellLogListener.php
@@ -2,22 +2,12 @@
 
 namespace CoverGenius\Tinker\Shell;
 
-use Covergenius\Php\Logger\Resources\CodeExecutionLogger;
+use Covergenius\Php\Logger\Resources\ArbitraryDataLogger;
 use Psy\ExecutionLoop\AbstractListener;
 use Psy\Shell;
 
 class ShellLogListener extends AbstractListener
 {
-    /**
-     * @var CodeExecutionLogger|null
-     */
-    protected ?CodeExecutionLogger $logger;
-
-    public function __construct()
-    {
-        $this->logger = new CodeExecutionLogger();
-    }
-
     /**
      * Listen for code execution.
      *
@@ -27,7 +17,10 @@ class ShellLogListener extends AbstractListener
      */
     public function onExecute(Shell $shell, string $code): void
     {
-        $this->logger->addToLog($code);
+        ArbitraryDataLogger::log([
+            'source' => 'tinker',
+            'code' => $code
+        ]);
     }
 
     /**
@@ -38,18 +31,5 @@ class ShellLogListener extends AbstractListener
     public static function isSupported(): bool
     {
         return true; //implement a proper check
-    }
-
-    /**
-     * Commits the log
-     *
-     * This ensures that the log is always saved, even if the shell is exited
-     * or the process is terminated.
-     *
-     * @return void
-     */
-    public function __destruct()
-    {
-        $this->logger->commitLog();
     }
 }
